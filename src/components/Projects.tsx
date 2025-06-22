cb6a7cce1b111a3007ecb06c287a9372e5ae2463
Zak <zak.hajjaji@icloud.com>
2025-06-23 00:36:17 +0100

    project card update for responsiveness

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -75,7 +75,7 @@ export const Projects = () => {
     </p>
     </div>
   
-<div  className="max-w-6xl mx-auto grid grid-cols-2 md:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project, index) => (
   <ProjectCard key={index} {...project} />
 ))}
