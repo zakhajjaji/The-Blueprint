3079b91e2d59ba979447bad5d869a9830481438c
Zak <zak.hajjaji@icloud.com>
2025-08-07 18:08:04 +0100

    updated cv link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,10 @@
-'use client';
+"use client";
 import { useState } from "react";
 import ThemeToggle from "@/components/ThemeToggle";
 import { Button } from "./ui/button";
-import Link from 'next/link';
+import Link from "next/link";
 import Image from "next/image";
-import { Menu } from 'lucide-react';
-
+import { Menu } from "lucide-react";
 
 export default function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,31 +21,77 @@ export default function Nav() {
         />
       </Link>
 
-
       <div className="hidden md:flex gap-6 text-lg md:text-base font-medium items-center">
-      <Link href="#projects" className="hover:text-primary transition-colors">Projects</Link>
-      <Link href="#contact" className="hover:text-primary transition-colors">Contact</Link>
-        <Link href="#about" className="hover:text-primary transition-colors">About Me</Link>
-        <Link href="#testimonials" className="hover:text-primary transition-colors">Testimonials</Link>
+        <Link href="#projects" className="hover:text-primary transition-colors">
+          Projects
+        </Link>
+        <Link href="#contact" className="hover:text-primary transition-colors">
+          Contact
+        </Link>
+        <Link href="#about" className="hover:text-primary transition-colors">
+          About Me
+        </Link>
+        <Link
+          href="#testimonials"
+          className="hover:text-primary transition-colors"
+        >
+          Testimonials
+        </Link>
         <Button asChild size="sm">
-          <a href="/assets/ZAK-HAJJAJI-JNR.SOFTWARE-DEV-CV.pdf" target="_blank" rel="noopener noreferrer">CV</a>
+          <a
+            href="/assets/Zak Hajjaji CV - Software Dev. Green.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CV
+          </a>
         </Button>
         <ThemeToggle />
       </div>
-
 
       <Button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
         <Menu className="w-6 h-6 text-foreground" />
       </Button>
 
-
       {menuOpen && (
         <div className="absolute top-full right-2 mt-2 w-30 bg-background dark:bg-background shadow-lg rounded-lg flex flex-col gap-4 p-4 md:hidden text-base font-medium z-50">
-         <Link href="#projects" className="hover:text-primary transition-colors" onClick={() => setMenuOpen(false)}>Projects</Link>
-         <Link href="#contact" className="hover:text-primary transition-colors" onClick={() => setMenuOpen(false)}>Contact</Link>
-          <Link href="#about" className="hover:text-primary transition-colors" onClick={() => setMenuOpen(false)}>About Me</Link>
-          <Link href="#testimonials" className="hover:text-primary transition-colors" onClick={() => setMenuOpen(false)}>Testimonials</Link>
-          <a href="/assets/ZAK-HAJJAJI-JNR.SOFTWARE-DEV-CV.pdf" target="_blank" rel="noopener noreferrer" className="text-primary" onClick={() => setMenuOpen(false)}>CV</a>
+          <Link
+            href="#projects"
+            className="hover:text-primary transition-colors"
+            onClick={() => setMenuOpen(false)}
+          >
+            Projects
+          </Link>
+          <Link
+            href="#contact"
+            className="hover:text-primary transition-colors"
+            onClick={() => setMenuOpen(false)}
+          >
+            Contact
+          </Link>
+          <Link
+            href="#about"
+            className="hover:text-primary transition-colors"
+            onClick={() => setMenuOpen(false)}
+          >
+            About Me
+          </Link>
+          <Link
+            href="#testimonials"
+            className="hover:text-primary transition-colors"
+            onClick={() => setMenuOpen(false)}
+          >
+            Testimonials
+          </Link>
+          <a
+            href="/assets/ZAK-HAJJAJI-JNR.SOFTWARE-DEV-CV.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary"
+            onClick={() => setMenuOpen(false)}
+          >
+            CV
+          </a>
           <ThemeToggle />
         </div>
       )}
